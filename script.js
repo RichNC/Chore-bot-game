0521ebfc89c931b073e0aee9afc1654f3fcfccc8
Rich Cherry <rich.m.cherry@gmail.com>
2020-05-02 15:15:16 -0400

    door randomness working

--- script.js
+++ script.js
@@ -1,29 +1,40 @@
 const doorImage1 = document.querySelector('#door1');
-const doorImage2 = document.querySelector('#door2');
-const doorImage3 = document.querySelector('#door3');
-const botDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/robot.svg';
-const beachDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/beach.svg';
-const spaceDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/space.svg';
-doorImage1.onclick = () => {
-  doorImage1.src = botDoorPath;
-}
-doorImage2.onclick = () => {
-  doorImage2.src = beachDoorPath;
-}
-doorImage3.onclick = () => {
-  doorImage3.src = spaceDoorPath;
-}
+let doorImage2 = document.querySelector('#door2');
+let doorImage3 = document.querySelector('#door3');
+let botDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/robot.svg';
+let beachDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/beach.svg';
+let spaceDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/space.svg';
 let numClosedDoors = 3;
 let openDoor1;
 let openDoor2;
 let openDoor3;
+
+
 randomChoreDoorGenerator = () => {
-  choreDoor = Math.floor(Math.random()*2)
+  choreDoor = Math.floor(Math.random()*2);
   if(choreDoor === 0) {
-
+    openDoor1 = botDoorPath;
+    openDoor2 = beachDoorPath;
+    openDoor3 = spaceDoorPath;
   } else if(choreDoor === 1) {
-
-  } else {
-
+    openDoor2 = botDoorPath;
+    openDoor3 = beachDoorPath;
+    openDoor1 = spaceDoorPath;
+  } else { (choreDoor === 2)
+    openDoor3 = botDoorPath;
+    openDoor1 = beachDoorPath;
+    openDoor2 = spaceDoorPath;
   }
+}
+
+randomChoreDoorGenerator();
+
+doorImage1.onclick = () => {
+  doorImage1.src = openDoor1;
+}
+doorImage2.onclick = () => {
+  doorImage2.src = openDoor2;
+}
+doorImage3.onclick = () => {
+  doorImage3.src = openDoor3;
 }
